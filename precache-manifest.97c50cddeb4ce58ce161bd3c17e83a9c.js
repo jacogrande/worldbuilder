581ffa3b0ce638e482e8086f7735cee84d8662eb
jacogrande <prowelljackson@gmail.com>
2020-06-15 15:23:21 -0700

    Updates

---
+++ precache-manifest.97c50cddeb4ce58ce161bd3c17e83a9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880a4fa222d93c793a4f9b8d0b374b6a",
+    "url": "/worldbuilder/index.html"
+  },
+  {
+    "revision": "fed8d9836b5c43a94f5f",
+    "url": "/worldbuilder/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cdf88b8216a047a09721",
+    "url": "/worldbuilder/static/js/2.d512deb7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/worldbuilder/static/js/2.d512deb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fed8d9836b5c43a94f5f",
+    "url": "/worldbuilder/static/js/main.46d0349b.chunk.js"
+  },
+  {
+    "revision": "dcfbf8b182bb2fa0e7a8",
+    "url": "/worldbuilder/static/js/runtime-main.56b236a7.js"
+  }
+]);
